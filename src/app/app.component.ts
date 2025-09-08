7e5a0283ae902c81d24831e8a2773554f3ee6717
zerozane5758 <c14220203@john.petra.ac.id>
2025-09-08 12:52:14 +0700

    Coba UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OrderDashboardComponent } from './order-dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, OrderDashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
